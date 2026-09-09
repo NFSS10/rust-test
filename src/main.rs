c7fc7f8d1cca53a2a50c5aa3a27c7eaf04f15ba0
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2026-09-09 20:39:11 +0100

    feat: CLI usage setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 1 {
+        eprintln!("Error: Expected exactly one argument for the input CSV file.");
+        eprintln!("Usage: cargo run -- myfile.csv > out.csv");
+        process::exit(1);
+    }
+
+    let file_path = &args[0];
+    println!("Processing file: {}", file_path);
 }
